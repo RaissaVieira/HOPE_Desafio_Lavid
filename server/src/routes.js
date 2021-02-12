bfbe792b6d0afd38a60e3e5ecc641fd77da857a6
Raissa Vieira <raissavieira125@gmail.com>
2021-02-12 14:42:20 -0300

    feat: Adicao de rota de busca de usuario

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,13 @@
 const routes = require('express').Router();
-
+const authenticate = require('./middlewares/authenticate');
 const userController = require('./controller/userController');
 
 routes.post('/register', userController.Create);
 
 routes.post('/authentication', userController.Authentication);
+
+routes.use(authenticate);
+
+routes.get('/name', userController.Name);
 
 module.exports = routes;
