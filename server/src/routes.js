7c292d29ee3209c43d6a588d6815ae2bd4d66886
Raissa Vieira <raissavieira125@gmail.com>
2021-02-12 10:34:55 -0300

    feat: Adicao da rota de autenticacao

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,4 +4,6 @@ const userController = require('./controller/userController');
 
 routes.post('/register', userController.Create);
 
+routes.post('/authentication', userController.Authentication);
+
 module.exports = routes;
