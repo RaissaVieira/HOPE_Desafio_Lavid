32a23613d5c45272cf2b324afa2823062f01a3b2
Raissa Vieira <raissavieira125@gmail.com>
2021-02-12 14:46:32 -0300

    refactor: atualizacoes

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,13 @@
-import express from 'express'
-import cors from 'cors'
-import routes from './routes'
+const express = require('express');
+const routes = require('./routes');
+//const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(cors())
+//app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
-app.listen(3333)
+app.listen(3333);
