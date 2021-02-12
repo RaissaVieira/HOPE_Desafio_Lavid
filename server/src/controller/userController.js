5db0e3269f4c262ff4730891f501e831e6c99eef
Raissa Vieira <raissavieira125@gmail.com>
2021-02-12 10:34:12 -0300

    feat: Criacao da rota de autenticacao

--- server/src/controller/userController.js
+++ server/src/controller/userController.js
@@ -1,28 +1,61 @@
 const connection = require('../database/connections');
+const encryptPassword = require('../utils/encryptPassword');
+const generateJWT = require('../utils/jwt');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
     async Create (req, res) {
-        const {name, email, password} = req.body;
-
         try {
+            const {name, email, password} = req.body;
+            const passCrypted = encryptPassword.encrypt(password);
+
             const [result] = await connection('users')
                 .where('email', email)
-                .select('name')
+                .select('email');
 
             if(result){
-                return response.status(400).json({error: "Email já cadastrado"})
+                return res.status(400).json({error: "Email já cadastrado"})
             }
 
             await connection('users').insert({
                 name,
                 email,
-                password
-            })
+                password:passCrypted
+            });
 
-            return response.json({id})
+            return res.json({ message: "Usuário cadastrado" })
 
-        } catch (error) {
+        } catch (err) {
             return res.status(400).json({ error: "Falha no registro de usuário" });
+        }
+    },
+
+    async Authentication (req, res) {
+        try {
+            const {email, password} = req.body;
+
+            const user = await connection('users')
+                .where('email', email)
+                .select()
+                .first();
+
+            if(!user){
+                return res.status(400).json({error: "Usuário não encontrado"})
+            }
+
+            if (!await bcrypt.compare(password, user.password)) {
+                return res.status(400).json({ error: "Invalid password" });
+            }
+
+            const token = generateJWT({id: user.id});
+
+            return res.json({
+                user,
+                token: token
+            });
+
+        } catch (err) {
+            return res.status(400).json({ error: "Falha na autenticação do usuário" });
         }
     }
 }
