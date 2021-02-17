c1e2901ff267dc0545c1a48768b634ba9a695210
Raissa Vieira <raissavieira125@gmail.com>
2021-02-17 14:17:38 -0300

    feat: Add funcao de captar token e limpar storage

--- mobile/src/pages/Welcome/index.js
+++ mobile/src/pages/Welcome/index.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Button, AsyncStorage } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import styles from './styles.js'
 import Header from '../../components/Header';
+import api from '../../services/api.js';
 
 export default function Welcome() {
 
   const navigation = useNavigation();
 
+  const [name, setName] = useState([]);
+
+  const Token = async () => {
+    const value = await AsyncStorage.getItem('Token')
+
+    return value;
+  }
+  
+  Token().then( value => {
+    api.get('name', {
+      headers: {
+        Authorization: `Bearer ${value}`
+      }
+    }).then(response => {
+      setName(response.data.name)
+    }) 
+  })
+  
   function handleGoToBackLogin() {
+    AsyncStorage.clear();
+
     navigation.navigate('Login');
   }
 
   return (
     <View style={styles.container}>
-      <Header text="Bem vindx, Raissa" />
+      <Header text={`Bem vindx, ${name}`} />
       <Button onPress={handleGoToBackLogin} title="Sair" color='#000000'/>
     </View>
   );
